ada1bce19f5c5611cdc6bb1b16588c85a42368b1
jay <jay@jays-MacBook-Air.local>
2025-08-16 01:14:57 -0400

    getting local tests ready

--- test/OVFL.t.sol
+++ test/OVFL.t.sol
@@ -3,11 +3,32 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {OVFL} from "../src/OVFL.sol";
+import {OVFLETH} from "../src/OVFLETH.sol";
 
 contract OVFLTest is Test {
+
+
     OVFL public ovfl;
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant PENDLE_ROUTER = 0x0000000000000000000000000000000000000000;
+    address public constant PENDLE_ORACLE = 0x0000000000000000000000000000000000000000;
+    address public constant PENDLE_MARKET = 0x0000000000000000000000000000000000000000;
+    address public constant PENDLE_SY = 0x0000000000000000000000000000000000000000;
+    address public constant PENDLE_PT = 0x0000000000000000000000000000000000000000;
+    address public constant PENDLE_PY = 0x0000000000000000000000000000000000000000;
+    address public constant PENDLE_LP = 0x0000000000000000000000000000000000000000;
+
+    address public constant ADMIN = address(0x123);
+    address public constant TREASURY = address(0x456);
 
     function setUp() public {
-        ovfl = new OVFL(address(this), address(this));
+        ovfl = new OVFL(ADMIN, TREASURY);
     }
+
+    // add tests for all functions
+    
+
+    
+
+    
 }
