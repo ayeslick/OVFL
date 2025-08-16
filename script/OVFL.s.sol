03ce83ca21f420239933baed1179d12fa7bee5b7
jay <jay@jays-MacBook-Air.local>
2025-08-15 23:07:25 -0400

    install needed files, update other files

--- script/OVFL.s.sol
+++ script/OVFL.s.sol
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {OVFL} from "../src/OVFL.sol";
 
-contract OVFLTest is Test {
+contract OVFLScript is Script {
     OVFL public ovfl;
 
-    function setUp() public {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
         ovfl = new OVFL(
             address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2),
             address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1),
             address(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1)
         );
+
+        vm.stopBroadcast();
     }
 }
--- src/OVFL.sol
+++ src/OVFL.sol
@@ -18,8 +18,23 @@ pragma solidity ^0.8.20;
  *       that acquires PT then calls deposit() and pays the fee.
  */
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {PRBMath} from "prb-math/PRBMath.sol";
+
 contract OVFL {
-    function get() public pure returns (uint256) {
-        return 1;
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable weth;
+    IERC20 public immutable peth;
+    IERC20 public immutable pt;
+
+    constructor(address _weth, address _peth, address _pt) {
+        weth = IERC20(_weth);
+        peth = IERC20(_peth);
+        pt = IERC20(_pt);
     }
 }
